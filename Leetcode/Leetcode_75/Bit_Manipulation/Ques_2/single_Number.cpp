6210b1fb1c91441e58b936323ed13ac657d94d31
RajAditya11 <rajadi7185@gmail.com>
2026-05-27 19:34:18 +0530

    Day-7 DSA part is done today i learned BFS on Tree and use of Monotonic Stack when looking for next greater/smaller element in the array.

---
+++ Leetcode/Leetcode_75/Bit_Manipulation/Ques_2/single_Number.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+
+        for( auto num : nums ){
+            res ^= num;
+        }
+
+        return res;
+    }
+};
+
+int main(){
+    return 0;
+}
